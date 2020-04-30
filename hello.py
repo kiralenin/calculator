4ea6820e0d9ddc8bd1036cfd1704a5db1ce70861
kiralenin <kirakirakira1060@gmail.com>
2020-04-30 17:04:23 +0300

    first

---
+++ hello.py
@@ -0,0 +1,41 @@
+act = input("Выберите действие:(%, /)")
+
+a = float(input("Укажи первое число: "))
+
+b = float(input("Укажи второе число: "))
+
+
+
+if act == "%":
+    c = a%b
+    print(c)
+    print(str(с))
+elif act == "/":
+    c = a / b
+    print("Результат: " + str(с))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# operators = ['+', '-', '/', '*', '%', '^']
+# def count(op):
+#     if (op == '+):
